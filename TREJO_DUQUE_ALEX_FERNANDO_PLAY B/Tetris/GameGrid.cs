a1fada544fec18c8b289de99950578f92f80f1e5
Alex Fernando Trejo Duque <aftrejo@espe.edu.ec>
2022-12-14 19:39:45 -0500

    Tetris

--- TREJO_DUQUE_ALEX_FERNANDO_PLAY B/Tetris/GameGrid.cs
+++ TREJO_DUQUE_ALEX_FERNANDO_PLAY B/Tetris/GameGrid.cs
@@ -1,4 +1,4 @@
-﻿namespace Tetris
+﻿/*namespace Tetris
 {
     public class Position
     {
@@ -11,8 +11,8 @@
             Column = column;
         }
     }
-}
-TREJO DUQUE ALEX FERNANDO :47
+}*/
+//TREJO DUQUE ALEX FERNANDO :47
 namespace Tetris
 {
     public class GameGrid
@@ -34,6 +34,77 @@ namespace Tetris
             grid = new int[rows, columns];
         }
 
-        public bool IsInside (int r, int c) { }
+        public bool IsInside(int r, int c)
+        {
+            return r >= 0 && r < Rows && c >= 0 && c < Columns;
+        }
+
+        public bool IsEmpty(int r, int c)
+        {
+            return IsInside(r, c) && grid[r, c] == 0;
+        }
+
+        public bool IsRowFull(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                if (grid[r, c] == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public bool IsRowEmpty(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                if (grid[r, c] != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void ClearRow(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r, c] = 0;
+            }
+        }
+
+        private void MoveRowDown(int r, int numRows)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r + numRows, c] = grid[r, c];
+                grid[r, c] = 0;
+            }
+        }
+
+        public int ClearFullRows()
+        {
+            int cleared = 0;
+
+            for (int r = Rows - 1; r >= 0; r--)
+            {
+                if (IsRowFull(r))
+                {
+                    ClearRow(r);
+                    cleared++;
+                }
+                else if (cleared > 0)
+                {
+                    MoveRowDown(r, cleared);
+                }
+            }
+
+            return cleared;
+        }
     }
 }
